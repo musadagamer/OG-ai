5aaa3ee8400da9ddc2d89fe77e26bf1a7235ee00
musadagamer <musatoseef10@gmail.com>
2026-06-27 00:45:55 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -57,13 +57,9 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
-
-    event.respondWith(
-
-        caches.match(event.request)
-
-        .then(response => response || fetch(event.request))
-
-    );
-
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
 });
