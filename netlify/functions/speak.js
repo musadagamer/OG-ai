53d40d4506725b5fc75a379f3279b447da25a30a
musadagamer <musatoseef10@gmail.com>
2026-06-28 13:19:19 +0500

    Update model_id to eleven_turbo_v2_5

--- netlify/functions/speak.js
+++ netlify/functions/speak.js
@@ -21,7 +21,7 @@ exports.handler = async function(event) {
       },
       body: JSON.stringify({
         text: text,
-        model_id: 'eleven_monolingual_v1',
+        model_id: 'eleven_turbo_v2_5',
         voice_settings: { stability: 0.4, similarity_boost: 0.8 }
       })
     });
